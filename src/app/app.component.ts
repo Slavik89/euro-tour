9946c44cb0e5638f10216774f43a5260dc02df17
Slavik89 <terakrokos@gmail.com>
2021-03-09 20:12:40 +0200

    Finish of the chapter #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Tour de France';
 
-  ngOnInit() {
-    console.log('hello dream!');
+  ngOnInit() {    
   }
 }
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,16 +13,17 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];  
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}   
+  constructor(private heroService: HeroService, private messageService: MessageService) {}   
 
   ngOnInit(): void {
     this.getHeroes();
-    console.log(this.selectedHero);
+    // console.log(this.selectedHero);
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log(this.selectedHero);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    // console.log(this.selectedHero);
   } 
 
   getHeroes(): void {
